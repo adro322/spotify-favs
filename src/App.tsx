52f005af19292d8a98d48879e84dc06fd81381c9
adro322 <aazuloagaaybar@gmail.com>
2025-11-07 22:56:35 -0500

    conexion api spo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (code) {
+      fetch("/api/callback", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("Token:", data.access_token);
+          // Aquí puedes guardar el token en localStorage o state
+        });
+    }
+  }, []);
+return(
+  <div className="p-10 text-white bg-gray-900 min-h-screen">
+      <a
+        href={`https://accounts.spotify.com/authorize?client_id=${
+          import.meta.env.VITE_SPOTIFY_CLIENT_ID
+        }&response_type=code&redirect_uri=https://spotify-favs-iota.vercel.app&scope=user-top-read`}
+        className="px-6 py-3 bg-green-500 rounded-lg hover:bg-green-600"
+      >
+        Conectar con Spotify
+      </a>
+    </div>
+  );
+
+
 }
 
 export default App

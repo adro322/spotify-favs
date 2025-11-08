b015f3c0fdf90efc85054e772311c2a63f83fd5a
adro322 <aazuloagaaybar@gmail.com>
2025-11-07 22:49:05 -0500

    creacion api

---
+++ api/callback.js
@@ -0,0 +1,33 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { code } = await req.json();
+
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+  const redirect_uri = "https://spotify-favs-iota.vercel.app";
+
+  const creds = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+
+  try {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${creds}`,
+      },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        code,
+        redirect_uri,
+      }),
+    });
+
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: "Error fetching token", details: err });
+  }
+}
